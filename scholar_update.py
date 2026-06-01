333d28eb6d91389dd3e5d4b494713ac2bc6196c6
Himanshu Chaube <102573714+himanshuchaube@users.noreply.github.com>
2026-06-02 04:04:53 +0530

    Update scholar_update.py

--- scholar_update.py
+++ scholar_update.py
@@ -4,23 +4,24 @@ import json
 SCHOLAR_ID = "n0ggSaUAAAAJ"
 
 author = scholarly.search_author_id(SCHOLAR_ID)
-
 author = scholarly.fill(author)
 
 stats = {
-"citations": author["citedby"],
-"h_index": author["hindex"],
-"i10_index": author["i10index"]
+    "citations": author["citedby"],
+    "h_index": author["hindex"],
+    "i10_index": author["i10index"]
 }
 
-with open("json_data/scholar_stats.json",
-"w",
-encoding="utf-8") as f:
+with open(
+    "json_data/scholar_stats.json",
+    "w",
+    encoding="utf-8"
+) as f:
 
-json.dump(
-    stats,
-    f,
-    indent=4
-)
+    json.dump(
+        stats,
+        f,
+        indent=4
+    )
 
-print("Scholar stats updated")
+print("Scholar stats updated successfully")
